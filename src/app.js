fa711061040b0658783e13fbbdbea968353d13ef
Taiki Arakawa <at.090313@gmail.com>
2016-08-05 15:39:03 +0900

    背景変更

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ var mylabel;
 //背景スクロールで追加した部分
 var gameLayer;
 var background;
+var rock_above;
+var rock_under;
+var ceiling;
+var land;
 var scrollSpeed = 1;
+var scrollSpeed2 = 1.5;
+var scrollSpeed3 = 2;
 var shrimp;
 var gameGravity = -0.05;
 var gameThrust = 0.1;
@@ -45,6 +51,22 @@ var game = cc.Layer.extend({
         background = new ScrollingBG();
         this.addChild(background);
 
+        //スクロールする背景スプライトをインスタンス2　スクロール速度:scrollSpeed2
+        rock_above = new ScrollingRA();
+        this.addChild(rock_above);
+
+        //スクロールする背景スプライトをインスタンス3　スクロール速度:scrollSpeed2
+        rock_under = new ScrollingRU();
+        this.addChild(rock_under);
+
+        //スクロールする背景スプライトをインスタンス4　スクロール速度:scrollSpeed3
+        ceiling = new ScrollingCE();
+        this.addChild(ceiling);
+
+        //スクロールする背景スプライトをインスタンス5　スクロール速度:scrollSpeed2
+        land = new ScrollingLA();
+        this.addChild(land);
+
         shrimp = new Shrimp();
         this.addChild(shrimp);
 
@@ -53,8 +75,12 @@ var game = cc.Layer.extend({
 
     },
     update:function(dt){
-      //backgroundのscrollメソッドを呼び出す
+      //background・その他のscrollメソッドを呼び出す
         background.scroll();
+        rock_above.scroll();
+        rock_under.scroll();
+        ceiling.scroll();
+        land.scroll();
         shrimp.updateY();
 
     },
@@ -78,7 +104,99 @@ var ScrollingBG = cc.Sprite.extend({
         this.setPosition(this.getPosition().x-scrollSpeed,this.getPosition().y);
         //画面の端に到達したら反対側の座標にする
         if(this.getPosition().x<0){
-            this.setPosition(this.getPosition().x+480,this.getPosition().y);
+            this.setPosition(this.getPosition().x+320,this.getPosition().y);
+        }
+    }
+});
+
+//スクロール移動する岩クラス1
+var ScrollingRA = cc.Sprite.extend({
+    //ctorはコンストラクタ　クラスがインスタンスされたときに必ず実行される
+    ctor:function() {
+        this._super();
+        this.initWithFile(res.rock_above_png);
+    },
+    //onEnterメソッドはスプライト描画の際に必ず呼ばれる
+    onEnter:function() {
+      //背景画像の描画開始位置 横960の画像の中心が、画面の端に設置される
+      this.setPosition(size.width,size.height-82 );
+      //  this.setPosition(480,160);
+    },
+    scroll:function(){
+      //座標を更新する
+        this.setPosition(this.getPosition().x-scrollSpeed2,this.getPosition().y);
+        //画面の端に到達したら反対側の座標にする
+        if(this.getPosition().x<0){
+            this.setPosition(this.getPosition().x+320,this.getPosition().y);
+        }
+    }
+});
+
+//スクロール移動する岩クラス2
+var ScrollingRU = cc.Sprite.extend({
+    //ctorはコンストラクタ　クラスがインスタンスされたときに必ず実行される
+    ctor:function() {
+        this._super();
+        this.initWithFile(res.rock_under_png);
+    },
+    //onEnterメソッドはスプライト描画の際に必ず呼ばれる
+    onEnter:function() {
+      //背景画像の描画開始位置 横960の画像の中心が、画面の端に設置される
+      this.setPosition(size.width,117 );
+      //  this.setPosition(480,160);
+    },
+    scroll:function(){
+      //座標を更新する
+        this.setPosition(this.getPosition().x-scrollSpeed2,this.getPosition().y);
+        //画面の端に到達したら反対側の座標にする
+        if(this.getPosition().x<0){
+            this.setPosition(this.getPosition().x+320,this.getPosition().y);
+        }
+    }
+});
+
+//スクロール移動する地面クラス1
+var ScrollingCE = cc.Sprite.extend({
+    //ctorはコンストラクタ　クラスがインスタンスされたときに必ず実行される
+    ctor:function() {
+        this._super();
+        this.initWithFile(res.ceiling_png);
+    },
+    //onEnterメソッドはスプライト描画の際に必ず呼ばれる
+    onEnter:function() {
+      //背景画像の描画開始位置 横960の画像の中心が、画面の端に設置される
+      this.setPosition(size.width,size.height-57 );
+      //  this.setPosition(480,160);
+    },
+    scroll:function(){
+      //座標を更新する
+        this.setPosition(this.getPosition().x-scrollSpeed3,this.getPosition().y);
+        //画面の端に到達したら反対側の座標にする
+        if(this.getPosition().x<0){
+            this.setPosition(this.getPosition().x+320,this.getPosition().y);
+        }
+    }
+});
+
+//スクロール移動する地面クラス2
+var ScrollingLA = cc.Sprite.extend({
+    //ctorはコンストラクタ　クラスがインスタンスされたときに必ず実行される
+    ctor:function() {
+        this._super();
+        this.initWithFile(res.land_png);
+    },
+    //onEnterメソッドはスプライト描画の際に必ず呼ばれる
+    onEnter:function() {
+      //背景画像の描画開始位置 横960の画像の中心が、画面の端に設置される
+      this.setPosition(size.width,57 );
+      //  this.setPosition(480,160);
+    },
+    scroll:function(){
+      //座標を更新する
+        this.setPosition(this.getPosition().x-scrollSpeed3,this.getPosition().y);
+        //画面の端に到達したら反対側の座標にする
+        if(this.getPosition().x<0){
+            this.setPosition(this.getPosition().x+320,this.getPosition().y);
         }
     }
 });
--- src/resource.js
+++ src/resource.js
@@ -1,6 +1,13 @@
 var res = {
     background_png : "res/background.png",
+    rock_above_png : "res/rock_above.png",
+    rock_under_png : "res/rock_under.png",
+    ceiling_png : "res/ceiling.png",
+    land_png : "res/land.png",
     shrimp0_png : "res/shrimp01.png",
+    shrimp1_png : "res/shrimp02.png",
+    shrimp2_png : "res/shrimp03.png",
+    shrimp3_png : "res/shrimp04.png",
     enemy0_png : "res/nagoya0.png",
     enemy1_png : "res/nagoya1.png",
     enemy2_png : "res/nagoya2.png",
